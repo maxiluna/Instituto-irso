71223d3bebbc6aef4038481c4677fb1f01c2eeba
Maxi <lunamaximiliano@gmail.com>
2021-08-10 22:51:08 -0300

    Update Unidad4-4.01-Vectores.c

--- 01-algebra/Unidad4-4.01-Vectores.c
+++ 01-algebra/Unidad4-4.01-Vectores.c
@@ -1,58 +1,37 @@
 #include<stdio.h>
  
- 
 int main(){
 	
-	float dimensiones
+	float dimensiones;
+	float vectx, vecty, vectresultadox, vectresultadoy, vectresultado;
+	int i;
+	vectresultadox = 0;
+	vectresultadoy = 0;
+	dimensiones = 0;
 	
 	printf("Vamos a sumar dos vector, agregaremos los coordenadas\n");
-	scanf("cuantas dimensiones tendra? ¿ 2 o 3 ? %f",&dimensiones);
+	scanf("cuantas dimensiones tendra? ¿ 2 o 3 ? %f",& dimensiones);
 	
-	float vectx, vecy, vectresultado;
-	int i;
+	
+	for(i=0;i<dimensiones-1;i++){
+		printf("Ingresamos las coordenadas del punto\n %f",& i+1);
+		
+		scanf("Ingrese punto x %f",& vectx);
+		scanf("Ingrese punto x %f",& vecty);
+		vectresultadox = vectx + vectresultadox;
+		vectresultadoy = vecty + vectresultadoy;
 
+		printf("%d %d", vectresultadox, vectresultadoy);
+	}
 /*
 	if dimensiones==2{
 		printf("introduce el valor de x para el vector A\n");
 		scanf("%f",&vect1x);
-	
-		printf("introduce el valor de y para el vector A\n");
-		scanf("%f",&vect1y);
 		
-		printf("introduce el valor de x para el vector B\n");
-		scanf("%f",&vect2x);
-	
-		printf("introduce el valor de y para el vector B\n");
-		scanf("%f",&vect2y);
-	} 
-	
-	if dimensiones==3{
-		printf("introduce el valor de x para el vector A\n");
-		scanf("%f",&vect1x);
-	
-		printf("introduce el valor de y para el vector A\n");
-		scanf("%f",&vect1y);
-		
-		printf("introduce el valor de x para el vector B\n");
-		scanf("%f",&vect2x);
-	
-		printf("introduce el valor de y para el vector B\n");
-		scanf("%f",&vect2y);
-		
-		printf("introduce el valor de x para el vector C\n");
-		scanf("%f",&vect3x);
-	
-		printf("introduce el valor de y para el vector C\n");
-		scanf("%f",&vect3y);
-	} 
+		printf("posicion x %2.0f y %2.0f\n", vectresultadox, vectresultadoy);
 
-
+	} 
 */
-	printf("******vector 3 es la suma de los 2 primeros vectores****\n");
-	for(i=0;i<dimensiones-1;i++){
-		vectresultado[i]=vect1[i]+vect2[i];
-		printf("posicion x %d -->%2.0f\n",i,vect3[i]);
-	}
-	
+
 	return 0;
 }
