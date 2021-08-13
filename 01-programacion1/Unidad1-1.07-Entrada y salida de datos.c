ea090e7f6c868a1fba35f9f6249f8c1d336d375f
Maxi <lunamaximiliano@gmail.com>
2021-08-13 09:15:10 -0300

    Create Unidad1-1.07-Entrada y salida de datos.c

---
+++ 01-programacion1/Unidad1-1.07-Entrada y salida de datos.c
@@ -0,0 +1,40 @@
+/*********************************************/
+/* Materia: Programacion 1	                 */
+/*                                           */
+/* Descripción: ENTRADA Y SALIDA             */
+/*              DE DATOS.					           */
+/*                                           */
+/* Autor: Maximiliano Luna                   */
+/*                                           */
+/*********************************************/
+
+/* Compiladores Online
+	https://www.onlinegdb.com/online_c_compiler
+	https://www.programiz.com/c-programming/online-compiler/
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE *archivo;
+	char caracter;
+	
+	archivo = fopen("archivodeprueba.txt","r");
+	
+	if (archivo == NULL)
+        {
+            printf("\nError de apertura del archivo. \n\n");
+        }
+        else
+        {
+            printf("\nEl contenido del archivo de prueba es \n\n");
+            while((caracter = fgetc(archivo)) != EOF)
+	    {
+		printf("%c",caracter);
+	    }
+        }
+        fclose(archivo);
+	return 0;
+}
