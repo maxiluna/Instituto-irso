5619887d91e3218e7ce1a63c9962cb0d1c436a53
Maxi <lunamaximiliano@gmail.com>
2021-08-17 18:10:04 -0300

    Create Unidad2-2.04-EstructurasDeDatos.c

---
+++ 01-programacion1/Unidad2-2.04-EstructurasDeDatos.c
@@ -0,0 +1,38 @@
+/*********************************************/
+/* Materia: Programacion 1                   */
+/*                                           */
+/* Descripción: ESTRUCTURAS                  */
+/*              DE DATOS.                    */
+/*                                           */
+/* Autor: Maximiliano Luna                   */
+/*                                           */
+/*********************************************/
+
+/* Compiladores Online
+	https://www.onlinegdb.com/online_c_compiler
+	https://www.programiz.com/c-programming/online-compiler/
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int x;
+
+main(){
+	/* 
+		printf:
+	*/
+	printf( "Primera Linea C\n" );
+	printf( "Segunda Linea C\n" );
+	printf("Tercera Linea\n.Cuarta Linea\n..Quinta Linea\n...Sexta Linea\n\n");
+
+	/* 
+		asignacion de variables y tipos:
+		printf
+			%c caracteres
+			%s cadena de caracteres
+			%d enteros
+			%f flotantes
+	*/
+	return 0;
+}
