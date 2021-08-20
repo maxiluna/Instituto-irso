7d5f37b30fa675f2db19899a65c4d5b4fa80a6b9
Maxi <lunamaximiliano@gmail.com>
2021-08-20 16:40:11 -0300

    Update Unidad2-2.04-EstructurasDeDatos.c

--- 01-programacion1/Unidad2-2.04-EstructurasDeDatos.c
+++ 01-programacion1/Unidad2-2.04-EstructurasDeDatos.c
@@ -16,23 +16,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int x;
-
 main(){
 	/* 
-		printf:
+	1 - División de enteros se realice en coma flotante
 	*/
-	printf( "Primera Linea C\n" );
-	printf( "Segunda Linea C\n" );
-	printf("Tercera Linea\n.Cuarta Linea\n..Quinta Linea\n...Sexta Linea\n\n");
+	
+	int x1=5,y1=2;
+	float f1;
+	printf("%d %d", x1, y1);
+	f1 = (float)x1/y1;
+	printf("\n%f", f1);
+	printf("\n------------------ 1 ------------------");
 
 	/* 
-		asignacion de variables y tipos:
-		printf
-			%c caracteres
-			%s cadena de caracteres
-			%d enteros
-			%f flotantes
+	2 - Flotante se convierte a entero
+	*/
+
+	float a2 = 5.25;
+	int b2;
+	b2 = (int)a2; /*En este caso, la variable b queda cargada con el valor 5*/
+	printf("\n%d", b2);
+	printf("\n%f", b2);
+	printf("\n------------------ 2 ------------------");
+	
+	/* 
+	3 - División de enteros se realice en coma flotante
+	*/
+	
+	int x3=7, y3=5 ;
+	float z3;
+	z3 = (float)x3/(float)y3; /*En muchos compiladores, z queda cargada con el valor 1*/
+	printf("\n%f", z3);
+	printf("\n------------------ 3 ------------------");
+
+
+
+	/* 
+	PAGINA 90
 	*/
 	return 0;
 }
