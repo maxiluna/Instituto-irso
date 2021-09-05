7866dc1dbd45084d1bee1901390a6f2dcbdbc363
Maxi <lunamaximiliano@gmail.com>
2021-09-05 15:32:08 -0300

    Add files via upload

---
+++ 01-programacion1/Unidad2-2.09-Ejercicio 6.c
@@ -0,0 +1,80 @@
+/*********************************************/
+/* Materia: Programacion 1                   */
+/*                                           */
+/* Descripción: MATRICES                     */
+/*                                           */
+/*                                           */
+/* Autor: Maximiliano Luna                   */
+/*                                           */
+/*********************************************/
+
+/* Compiladores Online
+	https://www.onlinegdb.com/online_c_compiler
+	https://www.programiz.com/c-programming/online-compiler/
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+main(){
+	/* 
+	Ejercicio 6: Realizar un programa que permita obtener la suma de dos
+	matrices X y J de dimensión axb (a=3 y b=4). Los elementos de la matriz son
+	reales. El resultado debe ser impreso. Realizar la suma componente a
+	componente. 
+	
+	Nota: Los valores de las matrices y los vectores deben ser cargados desde
+	teclado y siempre impresos sobre la pantalla.
+	
+	*/
+
+	int x, y;
+	int matrizX[3][4], matrizJ[3][4];
+
+	for(x=0; x<3; ++x) {
+		for(y=0; y<4; ++y){
+			printf("Ingrese un valor para MatrizX[%i][%i]: ", x, y);
+			scanf("%i", &matrizX[x][y]);
+			}
+	}
+
+	printf("\n");
+
+	for (x=0; x<3; ++x) {
+		for(y=0; y<4; ++y){
+
+			printf("Ingrese un valor para MatrizJ[%i][%i]: ", x, y);
+			scanf("%i", &matrizJ[x][y]);
+		}
+	}	
+
+	printf("\nMatrizX: \n\n");
+
+	for (x=0; x<3; x++) {
+		for (y=0; y<4; y++)
+		printf("%2d ", matrizX[x][y]);
+		printf("\n");
+	}
+
+	printf("\nMatrizJ: \n\n");
+	
+	for (x=0; x<3; x++) {
+		for (y=0; y<4; y++)
+		printf("%2d ", matrizJ[x][y]);
+		printf("\n");
+	}
+
+	printf("\nMatrizX + MatrizJ: \n\n");
+	
+	for (x=0; x<3; x++) {
+		for (y=0; y<4; y++)
+		printf("%2d ", matrizX[x][y]+matrizJ[x][y]);
+		printf("\n");
+	}
+	
+	printf("\n------------------ 6 ------------------");
+
+	/*PAGINA 112*/
+	return 0;
+}
