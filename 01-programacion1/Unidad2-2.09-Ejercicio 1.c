680fee8bf9ab2cbe403cdcd1479dacb099c09082
Maxi <lunamaximiliano@gmail.com>
2021-09-05 11:26:56 -0300

    Update Unidad2-2.09-Ejercicio 1.c

--- 01-programacion1/Unidad2-2.09-Ejercicio 1.c
+++ 01-programacion1/Unidad2-2.09-Ejercicio 1.c
@@ -1,8 +1,8 @@
 /*********************************************/
 /* Materia: Programacion 1                   */
 /*                                           */
-/* Descripción: MATRICES                     */
-/*                                           */
+/* Descripción: EJERCITACIÓN                 */
+/*              Vectores y matrices          */
 /*                                           */
 /* Autor: Maximiliano Luna                   */
 /*                                           */
@@ -15,3 +15,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+
+main(void){
+	/* 
+	Ejercicio 1: Realizar un programa que permita hacer nulos (cargarles valor 0)
+	los elementos de un vector Q de enteros, con n componentes (donde n es una
+	constante que vale 10). 
+
+	Nota: Los valores de las matrices y los vectores deben ser cargados desde
+	teclado y siempre impresos sobre la pantalla.
+	
+	*/
+	
+	const n = 10;
+	int x, vectorQ[n];
+	
+	for(x=0; x<n; ++x){
+		printf("Ingrese un valor para la posicion %i: ", x);
+		scanf("%i", &vectorQ[x]);
+	}		
+	
+	for(x=0; x<n; ++x) {
+		printf("\nEl valor de la posicion %i es: %i", x, vectorQ[x]);
+	}
+	printf("\n------------------ 1 ------------------");
+
+	/*PAGINA 112*/
+	return 0;
+}
