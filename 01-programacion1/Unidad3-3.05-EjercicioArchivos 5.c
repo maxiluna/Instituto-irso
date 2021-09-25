218f9b4ababe62dace563bc8d3632aa6c25662a4
Maxi <lunamaximiliano@gmail.com>
2021-09-25 14:47:40 -0300

    Create Unidad3-3.05-EjercicioArchivos 5.c

---
+++ 01-programacion1/Unidad3-3.05-EjercicioArchivos 5.c
@@ -0,0 +1,32 @@
+/*********************************************/
+/* Materia: Programacion 1                   */
+/*                                           */
+/* Descripción: EJERCICIOS                   */
+/*              ARCHIVOS                     */
+/*                                           */
+/* Autor: Maximiliano Luna                   */
+/*                                           */
+/*********************************************/
+
+/* Compiladores Online
+	https://www.onlinegdb.com/online_c_compiler
+	https://www.programiz.com/c-programming/online-compiler/
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+	Ejercicio 5: 
+	Dado un archivo de accidentes de tránsito, cuyos elementos son registros de tres campos: 
+		fecha (cadena de ocho caracteres),
+		cantidad de accidentados (entero) y 
+		código de zona (entero). 
+	El código de zona varía entre 1 y 20. 
+	
+	Realizar un programa que permita generar un vector donde se vayan 
+	acumulando la cantidad de accidentes por cada zona (el índice del vector 
+	coincide con el número de zona). 
+	Una vez obtenido el vector, imprimirlo.
+*/
