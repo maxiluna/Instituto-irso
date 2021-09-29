aca18b8ddcd28ba6ddf008ac6c6aa5948dcb5bb0
Maxi <lunamaximiliano@gmail.com>
2021-09-29 11:07:13 -0300

    Create Unidad4-4.02-Ejercicio1.c

---
+++ 01-programacion1/Unidad4-4.02-Ejercicio1.c
@@ -0,0 +1,24 @@
+/*********************************************/
+/* Materia: Programacion 1                   */
+/*                                           */
+/* Descripción: EJERCICIOS                   */
+/*              RECURSIVIDAD                 */
+/*                                           */
+/* Autor: Maximiliano Luna                   */
+/*                                           */
+/*********************************************/
+
+/* Compiladores Online
+	https://www.onlinegdb.com/online_c_compiler
+	https://www.programiz.com/c-programming/online-compiler/
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+	Ejercicio 1: 
+	Realizar un subprograma que permita calcular la siguiente serie:
+	a^n+a^(n-1)+a^(n-2)+...+a^2+a+1
+*/
