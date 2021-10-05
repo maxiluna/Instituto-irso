b649452cfbea6e47cf993308094c25546ccf2607
Maxi <lunamaximiliano@gmail.com>
2021-10-05 00:38:18 -0300

    Update Unidad4-4.02-Ejercicio7.c

--- 01-programacion1/Unidad4-4.02-Ejercicio7.c
+++ 01-programacion1/Unidad4-4.02-Ejercicio7.c
@@ -1,1 +1,51 @@
+/*********************************************/
+/* Materia: Programacion 1                   */
+/*                                           */
+/* Descripción: EJERCICIOS                   */
+/*              RECURSIVIDAD                 */
+/*                                           */
+/* Autor: Maximiliano Luna                   */
+/*                                           */
+/*********************************************/
 
+/* Compiladores Online
+	https://www.onlinegdb.com/online_c_compiler
+	https://www.programiz.com/c-programming/online-compiler/
+*/
+
+#include <stdio.h> 
+
+/*
+	Ejercicio 7: 
+	Calcular la potencia n de un número a (a^n), con n entero
+*/
+
+double a, n;
+double resultado;
+
+calculopotencia() {
+	// Copio posicion de string en letra a validar
+	printf("Ingrese numero a: ");
+	scanf("%lf", &a);
+	printf("Ingrese numero n: ");
+	scanf("%lf", &n);
+	// Verifico el largo del string e ingreso en recursion.
+	if (a <= 0 || n <= 0){
+		printf("Error, ingrese nuevamente...\n\n");
+		// Llama a la misma funcion
+		calculopotencia();
+	} else {
+	// Exponente por unica vez - a^n (pow(valora, valorn))
+	printf ( "Calculo = 'a': %.f exponente 'n': %.f\n", a, n );
+	resultado = pow(a, n);
+	}
+}
+
+int main()  
+{
+
+	// Llama a la funcion calculopotencia
+	calculopotencia();
+	printf("La potencia de a^n es = %.f\n", resultado);
+    return 0;
+} 
