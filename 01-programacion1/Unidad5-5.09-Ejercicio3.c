950cca755943fcf67f3369a440822c04c91b6147
Maxi <lunamaximiliano@gmail.com>
2021-10-09 11:24:49 -0300

    Create Unidad5-5.09-Ejercicio3.c

---
+++ 01-programacion1/Unidad5-5.09-Ejercicio3.c
@@ -0,0 +1,29 @@
+/*
+Los punteros permiten manipular la memoria del ordenador. 
+Todo dato está almacenado a partir de una dirección de memoria. 
+Esta dirección puede ser obtenida y manipulada también como un dato más. 
+Los punteros son importantes porque nos permiten manipular la memoria directamente.
+
+*/
+#include <stdio.h>
+ 
+int main(void) {
+ 
+int var1 = 2;
+int *ptr;
+
+printf ( "Valor de Variable VAR1: %d\n", var1);
+printf ( "Direccion de Variable VAR1: %p\n", &var1);
+printf ( "Direccion original de puntero: %p\n", &ptr);
+printf ( "Valor de puntero: %p\n", ptr);
+//printf ( "\nValor de lo que apunta el puntero: %d\n", *ptr);
+
+ptr = &var1;
+
+printf ( "\nDespues de realizar la asignacion ptr = &var1\n");
+printf ( "Direccion original de puntero: %p\n", &ptr);
+printf ( "Valor de puntero: %p\n", ptr);
+printf ( "Valor de lo que apunta el puntero: %d\n", *ptr);
+ 
+return 0;
+}
