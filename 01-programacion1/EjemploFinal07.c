b42da06d6d4c27979a7e8b8dc5d2ec8b5391647e
Maxi <lunamaximiliano@gmail.com>
2021-12-21 23:44:57 -0300

    Create EjemploFinal07.c

---
+++ 01-programacion1/EjemploFinal07.c
@@ -0,0 +1,22 @@
+/*
+Generar un programa que permita cargar valores enteros (int) de teclado y cargarlos
+en un archivo. La carga finaliza cuando se lee un valor 13(trece) o 15(quince).
+*/
+
+#include <stdio.h>
+
+int main(){
+	int x;
+	do {
+		printf("\n Ingrese un numero: ");
+		scanf("%i", &x);
+		FILE* fp;
+		fp = fopen("EjemploFinal07.txt", "w");
+		fprintf(fp, "\n%d\n", x);
+		printf(" %d\r\n", x);
+		fclose(fp);
+		printf("\nfclose(fp)");
+	} while ((x!=13)||(x!=15));
+	printf("\nFin");
+	return 0;
+}
