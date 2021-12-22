5d50d1bb2a2e79877cccdecd906950beaff74d4d
Maxi <lunamaximiliano@gmail.com>
2021-12-21 23:13:35 -0300

    Create EjemploFinal06.c

---
+++ 01-programacion1/EjemploFinal06.c
@@ -0,0 +1,32 @@
+/*
+Realizar una funcion que permita calcular la division entre dos valores enteros A y B.
+A debe ser mayor que B, y ademas, B debe ser diferente de 0 (cero) para que pueda
+realizarse el calculo. Incluir la invocacion de la funcion desde un programa.
+A y B se ingresan por teclado.
+*/
+
+#include <stdio.h>
+//Declado las varias globales
+int A, B, division=0;
+
+void invocarfuncion(A, B){
+	division = A/B;
+	printf("\n El valor de A es: %d", A);
+	printf("\n El valor de B es: %d", B);
+	printf("\n El valor de A / B es: %d", division);
+}
+
+int main(){
+	// Ingreso por teclado x, y
+	printf(" Ingrese un numero para 'A': ");
+	scanf("%i", &A);
+	printf("\n Ingrese un numero para 'B': ");
+	scanf("%i", &B);
+	// Ingreso por teclado x, y
+	if ((B!=0)&&(A<B)){
+		printf("B es igual a cero o A es menor a B");
+	} else {
+		invocarfuncion(A, B);
+	}
+	return 0;
+}
