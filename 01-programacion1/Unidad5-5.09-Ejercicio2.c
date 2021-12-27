da93199902cddead7a392e8da1229be4bec82ad3
Maxi <lunamaximiliano@gmail.com>
2021-12-27 15:06:33 -0300

    Create Unidad5-5.09-Ejercicio2.c

---
+++ 01-programacion1/Unidad5-5.09-Ejercicio2.c
@@ -0,0 +1,26 @@
+/*********************************************/
+/* Materia: Programacion 1                   */
+/*                                           */
+/* Descripción: EJERCICIOS                   */
+/*              PUNTEROS                     */
+/*                                           */
+/* Autor: Maximiliano Luna                   */
+/*                                           */
+/*********************************************/
+
+/* Compiladores Online
+	https://www.onlinegdb.com/online_c_compiler
+	https://www.programiz.com/c-programming/online-compiler/
+*/
+
+/*
+Ejercicio 2: 
+Realizar la declaración de dos variables de tipo entero. 
+Cargar el valor 1 a la primera variable y el valor 2 a la segunda. 
+Luego intercambiar los valores de la variable 1 en la 2 y la 
+variable 2 en al 1 usando punteros. 
+Imprimir los contenidos de las variables usando los punteros tanto 
+en la situación inicial como en la situación final.
+*/
+#include <stdio.h>
+
