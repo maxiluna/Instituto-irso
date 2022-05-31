ad4d9bfaa21fb2007b5ab5ed8ab69b1f4d88d5d5
Maxi <lunamaximiliano@gmail.com>
2022-05-31 20:50:25 -0300

    Update EjemploFinal07.c

--- 01-programacion1/EjemploFinal07.c
+++ 01-programacion1/EjemploFinal07.c
@@ -6,15 +6,21 @@ en un archivo. La carga finaliza cuando se lee un valor 13(trece) o 15(quince).
 #include <stdio.h>
 
 int main(){
-	int x;
-	do {
-		printf("\n Ingrese un numero: ");
-		scanf("%i", &x);
-		FILE* fp;
-		fp = fopen("EjemploFinal07.txt", "w");
-		fprintf(fp, "\n%d\n", x);
-		fclose(fp);
-	} while ((x!=13)||(x!=15));
+	int x, y;
+	FILE* fp;
+
+	for (y=0; y<2; y++){
+		if(x == 13 || x ==15){
+			break;
+		} else {
+			printf("\n Ingrese un numero: ");
+			scanf("%i", &x);
+			fp = fopen("EjemploFinal07.txt", "w");
+			fprintf(fp, "%d\n", x);
+			y--;
+		}
+	}
+	fclose(fp);
 	printf("\nFin");
 	return 0;
 }
